384c4cd1f87cf2014fe6489cd5aefd026d894804
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-05-18 21:29:44 +0000

    chore: created new files to the project

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,8 @@
+/**
+ * arquivo: routes/user.routes.ts
+ * descrição: arquivo responsável pelas rotas da api relacionado a classe 'User'
+ * data: 05/18/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+console.log('Hello World!');
